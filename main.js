c3b5152d3448a8d208bb6f36cb8845934aeb4627
Ethan Chapman <ethan@we3chapmans.com>
2021-08-16 20:58:59 -0400

    Added board and roll. Unfortunately no fancy roll.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var rs = "";
+var rsFull = "";
+
 window.addEventListener("DOMContentLoaded", event => {
 	var rulesetSelect = document.getElementById("ruleset");
 	var variationSelect = document.getElementById("variation");
@@ -26,20 +29,46 @@ window.addEventListener("DOMContentLoaded", event => {
 			
 			variationDiv.style.display = "block";
 		} else {
-			variationSelect.innerText = "";
+			variationSelect.innerHTML = "";
 			variationDiv.style.display = "none";
 		}
 	});
 	
 	start.addEventListener("click", e => {
-		var rs = rulesetSelect.value;
+		rs = rulesetSelect.value;
 		var varn = variationSelect.value;
-		var rsFull = rs;
+		rsFull = rs;
 		if(varn != "") {
 			rsFull += "-"+varn;
 		}
+		document.getElementById("game").style = "";
+		document.getElementById("startPage").className = "unloaded";
+		setTimeout(()=>{document.getElementById("game").className = "loaded";}, 0);
 		console.log(rsFull);
+		setTimeout(rollDice, 1000);
 	});
 	
 	console.log("Loaded!");
 });
+
+function rollDice() {
+	var p1die = document.getElementById("p1die");
+	var p2die = document.getElementById("p2die");
+	var p1fn = Math.ceil(Math.random()*6);
+	var p2fn = Math.ceil(Math.random()*6);
+	p1die.innerHTML = '<img src="dice/d6-d-'+p1fn+'.svg" alt="Player 1\'s die showing '+p1fn+'" width=64px/>';
+	p2die.innerHTML = '<img src="dice/d6-d-'+p2fn+'.svg" alt="Player 2\'s die showing '+p2fn+'" width=64px/>';
+	
+	document.getElementById("rollSound").play();
+	if(p1fn == p2fn) {
+		setTimeout(rollDice, 1500);
+	} else {
+		setTimeout(initGame, 2500, p1fn > p2fn);
+	}
+}
+
+function initGame(p1Start) {
+	console.log("Player "+(p1Start ? "1" : "2")+" is starting first");
+	document.getElementById("p1die").style.display = "none";
+	document.getElementById("p2die").style.display = "none";
+}
